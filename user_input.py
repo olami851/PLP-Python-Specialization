435917698cd3d0e05dca13df3c4d44cc87ec9b5a
olami851 <abdhamid851@gmail.com>
2024-11-07 09:43:37 +0100

    week 1 python specialization assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: \n")
+
+age = input('Enter your age: \n')
+
+location = input('Enter your location: \n')
+
+print(f"Hello {name}, you are {age} years old, and you have been living at {location} for the past 5 years now")
