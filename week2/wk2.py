5577a7628efb6770292a9e247f5a1c617b438cac
olami851 <abdhamid851@gmail.com>
2024-11-15 05:10:37 +0100

    added week 2 assignment to my repo

---
+++ week2/wk2.py
@@ -0,0 +1,39 @@
+# Creation of an empty list with my_list as it's name
+
+my_list = []
+print(my_list)
+
+# append elements into my_list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)
+
+#insertion of 15 into at the second position of mt list item
+
+my_list.insert(1, 15)
+print(my_list)
+
+# Extending my list item with the following numbers
+list2 = [50, 60, 70]
+my_list.extend(list2)
+print(my_list)
+
+# Removing last element in my list item
+
+my_list.pop()
+
+print(my_list)
+
+# Sort list items in ascending order
+
+my_list.sort()
+
+print(my_list)
+
+# Find and print the index of 30
+
+print(my_list [3])
